39b782d7902b92ce04c80639de6ddf308b402c28
bryan <thenhernandezb@gmail.com>
2022-07-17 04:32:40 -0400

    style: tweaks on container

--- src/services.ts
+++ src/services.ts
@@ -5,16 +5,18 @@ export function getNotes(): Array<Note> {
   return JSON.parse(localStorage.getItem('notes') ?? '[]')
 }
 
-export function saveNote(note: Omit<Note,'id'>) {
+export function saveNote(note: Omit<Note, 'id'>) {
   const id = UUIDClass.genV4().toString()
   const notes = [...getNotes(), { ...note, id }]
   localStorage.setItem('notes', JSON.stringify(notes))
 }
 
 export function deleteNote(id: string) {
-  
-  
   const notes = getNotes().filter((note) => note.id !== id)
-
   localStorage.setItem('notes', JSON.stringify(notes))
+}
+
+export function updateNote(note: Note) {
+  const notes = getNotes().filter((n) => n.id !== note.id)
+  localStorage.setItem('notes', JSON.stringify([...notes, note]))
 }
