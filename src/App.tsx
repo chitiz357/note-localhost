231ee03ab3b4c8b72dba71be0633c8fdf3cca1b5
bryan <thenhernandezb@gmail.com>
2022-07-17 00:36:34 -0400

    refactor: remove no use components

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,18 @@
 import React from 'react'
-import { NoteForm , NoteList} from './components'
-import { Note } from './models'
+import { NoteForm, NoteList } from './components'
 import { getNotes } from './services'
 
-// const notes = [
-//   {tittle:'1',content:'hola mundo'}
-//   ,{tittle: '2' , content: 'local storage'}
-// ]
-// localStorage.setItem('notes', JSON.stringify(notes))
 
 function App() {
   const notes = getNotes()
   console.log(notes)
 
   return (
-      <main>
+    <main>
       <NoteForm />
       <NoteList notes={notes} />
-      </main>
+    </main>
   )
 }
-
-
-function NotesList({ notes }: { notes: Note[] }) {
-  return (
-    <div>
-      {notes.map((note) => (
-        <NoteCard note={note} key={Math.random()} />
-      ))}
-    </div>
-  )
-}
-
-function NoteCard({ note }: { note: Note }) {
-  return (
-    <div>
-      <h3>{note.tittle}</h3>
-      <p>{note.content}</p>
-    </div>
-  )
-}
-
-
 
 export default App
